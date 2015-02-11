317496bc4e203c87e73a71b6f80d30f3334bcf40
Nathan Harms <nathanharms@gmail.com>
2015-02-10 23:02:59 -0700

    Added a test program using Vty-ui for the graphics.

---
+++ src/test.hs
@@ -0,0 +1,39 @@
+import Graphics.Vty.Widgets.All
+import qualified Data.Text as T
+
+main::IO ()
+main = do
+  -- The player types here.
+  input <- editWidget
+  binput <- bordered =<< (return input)
+
+  -- The title of... something?
+  title <- plainText (T.pack "Game time!")
+
+  -- The responses will go in here.
+  output <- plainText (T.pack "This is where the output goes.")
+  boutput <- bordered =<< (return output)
+
+  -- The inventory.
+  inventory <- plainText (T.pack "This is where the inventory goes.")
+  binventory <- bordered =<< (return inventory)
+
+  -- The window where all the descriptive stuff will be.
+  stateWindow <- hBox boutput binventory
+  ui <- (return title) <--> hBorder <--> (return stateWindow) <--> (return binput)
+
+  fg <- newFocusGroup
+  addToFocusGroup fg input
+
+  c <- newCollection
+  addToCollection c ui fg
+
+  -- Process input.
+  input `onActivate` \this ->
+    getEditText this >>= (error . getResponse . T.unpack)
+
+  -- Loop.
+  runUi c defaultContext
+
+getResponse::String->String
+getResponse _ = "You are a bad player."
