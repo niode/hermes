57684cbe7292542f78d6ea3ff9d6f53d4eb3e338
schornT <schorn.t@gmail.com>
2015-03-30 22:57:50 -0600

    More tests

--- src/unitTests.hs
+++ src/unitTests.hs
@@ -1,14 +1,24 @@
+module UnitTests where
+
 import Test.HUnit
 import Parser
+import Items
+
 
 --run all tests
 main = runTestTT allTests
 
 --list of all tests
-allTests = TestList [TestLabel "parseCommandAVerbTest" parseCommandAVerbTest, TestLabel "parseCommandAVerbTest2" parseCommandAVerbTest2]
+allTests = TestList [
+  TestLabel "parseCommandAVerbTest" parseCommandAVerbTest,
+  TestLabel "parseCommandAVerbTest2" parseCommandAVerbTest2,
+  TestLabel "alternateTest" alternateTest
+  ]
 
 
 --individual tests. Format is: assertEqual <Error message> <Expected> <Actual>
+
+--parser tests
 parseCommandAVerbTest = TestCase $ assertEqual
   "Should get a Verb Action from parseCommand" (AVerb (Do (NounConst "thing") POn (NounConst "that"))) (parseCommand "do thing on that")
 
@@ -17,7 +27,15 @@ parseCommandAVerbTest2 = TestCase $ assertEqual
 
 
 
+--items tests
+alternateTest = TestCase $ assertEqual
+  "Proper alternate" ([4,1,5,2,6,3]) (alternate [1,2,3] [4,5,6])
 
+--combineTest = TestCase $ assertEqual
+--  "Should return an Item" ([Fly,Rad,Fly,Rad]) (combine [Fly, Rad] [Fly, Rad])
+
+--nounifyTest = TestCase $ assertEqual
+--  "Should return a list of Strings" (["fins", ""]) (nounify ([Fly], Fly, [Fly]))
 
 
 
