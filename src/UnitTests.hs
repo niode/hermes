8f77f533bdd5747d2354b159dc8d23a650f4db12
Tristan Schorn <schorn.t@gmail.com>
2015-04-02 21:56:15 -0600

    Lots of unit tests

--- src/UnitTests.hs
+++ src/UnitTests.hs
@@ -16,7 +16,16 @@ allTests = TestList [
   TestLabel "parseCommandAVerbTest2" parseCommandAVerbTest2,
   TestLabel "combineTest" combineTest,
   TestLabel "nounifyTest" nounifyTest,
-  TestLabel "describeTest" describeTest
+  TestLabel "describeTest" describeTest,
+  TestLabel "describeTest2" describeTest2,
+  TestLabel "describeTest3" describeTest3,
+  TestLabel "describeTest4" describeTest4,
+  TestLabel "describeTest5" describeTest5,
+  TestLabel "describeTest6" describeTest6,
+  TestLabel "describeTest7" describeTest7,
+  TestLabel "describeTest8" describeTest8,
+  TestLabel "describeTest8" describeTest9,
+  TestLabel "describeTest8" describeTest10
   
   ]
 
@@ -35,6 +44,10 @@ parseCommandAVerbTest2 = TestCase $ assertEqual
   "Should get a Verb Action from parseCommand" (AVerb (UseTarget (NounConst "FlashDrive") POn (NounConst "self"))) (parseCommand "use FlashDrive on self")
 
 --engine tests
+runCommandTest = TestCase $ assertEqual
+  "Should return State GameState UIResponse" (Nothing) (runCommand (AVerb (Do (NounConst "thing") POn (NounConst "that"))))
+
+
 --uiDescriptionTest = TestCase $ assertEqual
 --  "Should return a ui description" (Just "not correct") (uiDescription (UIDString "test"))
 
@@ -51,7 +64,34 @@ parseCommandAVerbTest2 = TestCase $ assertEqual
 
 --items tests
 describeTest = TestCase $ assertEqual
+  "Should return an Item description" (Just "grabber") (describe [Grasp])
+
+describeTest2 = TestCase $ assertEqual
   "Should return an Item description" (Just "lighter-than-air fins") (describe [Fly])
+
+describeTest3 = TestCase $ assertEqual
+  "Should return an Item description" (Just "pouch") (describe [Carry])
+
+describeTest4 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Explode])
+
+describeTest5 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Fire])
+
+describeTest6 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Illuminate])
+
+describeTest7 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Upgrade])
+
+describeTest8 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Expend 2])
+
+describeTest9 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [Break])
+
+describeTest10 = TestCase $ assertEqual
+  "Should return an Item description" (Nothing) (describe [System])
 
 combineTest = TestCase $ assertEqual
   "Should return an Item" ([Magnify 2,Fly,Fly,Rad,Rad]) (combine [Fly, Rad] [Fly, Rad])
