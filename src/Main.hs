c044aaef16f90358a2f4939e669bc1f7d3f74b2b
Nathan <nathanharms@gmail.com>
2015-04-02 21:09:58 -0600

    Comments.

--- src/Main.hs
+++ src/Main.hs
@@ -7,14 +7,17 @@ import Engine
 
 newtype GameStateW = GameStateW GameState
 
+-- Generate a new game state widget.
 newGameState :: GameState -> IO (Widget GameStateW)
 newGameState state = do
   let st = GameStateW state
   newWidget st $ \w -> w
 
+-- Set the current game state.
 setGameState :: Widget GameStateW -> GameState -> IO ()
 setGameState wRef val = updateWidgetState wRef $ const $ GameStateW val
 
+-- Get the current game state.
 getGameState :: Widget GameStateW -> IO GameState
 getGameState wRef = do
   GameStateW state <- getState wRef
@@ -55,23 +58,26 @@ main = do
   c <- newCollection
   addToCollection c ui fg
 
+  -- Update the interface with the initial state.
   updateUI output inventory input gameState $ wrapResponse gameState ""
 
   -- Process input.
   input `onActivate` \this ->
     getEditText this >>=
-      ((updateUI output inventory input gameState) .
-      (wrapResponse gameState) . 
-      T.unpack)
+      ((updateUI output inventory input gameState) . -- Update UI.
+      (wrapResponse gameState) .  -- Get the engine's response.
+      T.unpack) -- Unpack Text.
 
   -- Loop.
   runUi c defaultContext
 
+-- Wrap the response function in the IO monad.
 wrapResponse:: Widget GameStateW -> String -> IO (UIResponse, GameState)
 wrapResponse gs cmd = do
   st <- getGameState gs
-  return $ runState (getResponse cmd) st
+  return $ runState (getResponse cmd) st -- Run in the State GameState monad.
 
+-- Update the UI (requires the description, inventory, input and game state).
 updateUI:: Widget FormattedText
         -> Widget FormattedText
         -> Widget Edit
@@ -86,12 +92,14 @@ updateUI desc inv input state response = do
   setEditText input (T.pack "")
   return ()
 
+-- Update the description widget with the UIDescriptionResponse.
 updateDescription::Widget FormattedText -> Maybe UIDescriptionResponse -> IO ()
 updateDescription desc Nothing = return ()
 updateDescription desc (Just resp) = case resp of
   UIDExit -> shutdownUi
   UIDString s -> setText desc (T.pack s)
 
+-- Update the inventory widget with the UIInventoryResponse.
 updateInventory::Widget FormattedText -> Maybe UIInventoryResponse -> IO ()
 updateInventory inv Nothing = return ()
 updateInventory inv (Just resp) = case resp of
